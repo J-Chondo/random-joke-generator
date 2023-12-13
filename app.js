ea2e87a437971e34da6e6bf3f19a040f48ecde30
CHONDO DIO <you@example.com>
2023-12-13 14:27:55 +0300

    adding partials and finializing

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
  const express = require("express");
  const axios = require("axios");
-
-
-
-
  const app = express();
  const port = process.ENV || 5000;
 
@@ -16,28 +12,26 @@ app.use(express.static("public"))
 // render the default index 
 
 app.get("/", (req, res) => {
-    res.render("index", {Joke: null} )
+    res.render("index", { joke: null,})
 })
 
 // handing the submit route
 app.post("/submit", async (req, res)=>{
     
-// fetching data from the api
-const APIurl = 'https://v2.jokeapi.dev/joke/[Category/-ies]'
+// fetching data from the api using axios
+const APIurl = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 try {
     const result = await axios.get(APIurl);
     res.render("index", {
-        Joke: result.data.Joke
+        joke: result.data.joke,
     })
 }catch(error){
     console.log(error.message)
     res.status(500)
 }
-
-
 });
 
-
+// setting to listen at port 5000
  app.listen(port, () => {
     console.log("server running on port 5000")
  });
