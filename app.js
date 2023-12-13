fb74ce4e852677d3171571352d2e2a5a8191fb1f
CHONDO DIO <you@example.com>
2023-12-13 10:45:38 +0300

    working on the ejs file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 
 
 
+
  const app = express();
  const port = process.ENV || 5000;
 
@@ -12,6 +13,29 @@ app.set("view engine", "ejs");
 // serve the public folder as static file
 app.use(express.static("public"))
 
+// render the default index 
+
+app.get("/", (req, res) => {
+    res.render("index", {Joke: null} )
+})
+
+// handing the submit route
+app.post("/submit", async (req, res)=>{
+    
+// fetching data from the api
+const APIurl = 'https://v2.jokeapi.dev/joke/[Category/-ies]'
+try {
+    const result = await axios.get(APIurl);
+    res.render("index", {
+        Joke: result.data.Joke
+    })
+}catch(error){
+    console.log(error.message)
+    res.status(500)
+}
+
+
+});
 
 
  app.listen(port, () => {
